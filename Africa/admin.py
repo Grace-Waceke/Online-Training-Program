0ece6835d1fd0babe9f4d5bac6c07b343d2ae699
Grace-Waceke <gracewacekemwangi@gmail.com>
2022-08-02 21:14:54 +0300

    added trainer and courses models

--- Africa/admin.py
+++ Africa/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from.models import Student, Community,Trainer,Course
 
-# Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display=('first_name','last_name','age','email','date_of_birth','national_id','phone_number','guardian_name','guardian_contacts','country')
+    search_fields=('first_name','last_name','age','email','date of birth','national id','phone number','guardian name','guardian contacts','country')
+admin.site.register(Student,StudentAdmin)
+admin.site.register(Community)
+admin.site.register(Trainer)
+admin.site.register(Course)
+
+
+
+
--- Africa/models.py
+++ Africa/models.py
@@ -5,43 +5,70 @@ import datetime
 
 # Create your models here.
 class Community(models.Model):
-    community_id = models.IntegerField(max_length=200, default="123456")
-    title = models.CharField(max_length=200, default="Developer Skills")
-    champions = models.CharField(max_length=250, default="Lupita Tiara")
+    community_id = models.IntegerField()
+    title = models.CharField(max_length=200,null=True)
+    champions = models.CharField(max_length=250,null=True)
 
 
 
 class Student(models.Model):
-    first_name=models.CharField(max_length=10, default="Shanel")
-    last_name = models.CharField(max_length=10, default="Lopez",)
-    age = models.PositiveSmallIntegerField(default=29)
+    first_name=models.CharField(max_length=10)
+    last_name = models.CharField(max_length=10)
+    age = models.PositiveSmallIntegerField()
     date_of_birth = models.DateField()
-    national_id = models.CharField(max_length=9, default=867)
-    email = models.EmailField(default="shanelopez@gmail.com")
-    registration = models.PositiveIntegerField(default=1324)
-    phone_number = models.PositiveIntegerField(default=3211283957)
-    guardian_name = models.CharField(max_length=15,default="Murathi",null=True,blank=True)
-    guardian_contacts = models.PositiveIntegerField(default=2987653245)
-    country = models.CharField(max_length=20, default=40,null=True,blank=True)
-    # medical_report = models.FileField(default="default.png",null=True,blank=True)
-    # class_name = models.CharField(max_length=12, default=30,null=True,blank=True)
-    # room_number = models.CharField(max_length=15, default=60,null=True,blank=True)
-    # big_sister = models.CharField(max_length=20, default=40)
-    mentors_name = models.CharField(max_length=35, default=50,null=True,blank=True)
-    passport_photo = models.ImageField(default="default.jpeg",null=True,blank=True)
+    national_id = models.CharField(max_length=9)
+    email = models.EmailField()
+    # registration = models.PositiveIntegerField(registration,on_delete=models.CASCADE)
+    phone_number = models.PositiveIntegerField()
+    guardian_name = models.CharField(max_length=15)
+    guardian_contacts = models.PositiveIntegerField()
+    country = models.CharField(max_length=20)
+    medical_report = models.FileField(default="default.png",null=True)
+    class_name = models.CharField(max_length=12, default=30,null=True)
+    mentors_name = models.CharField(max_length=35, default=50,null=True)
+    passport_photo = models.ImageField(default="",null=True)
     profile=models.ImageField(default="default.jpng")
+# def __str__(self):
+#         return self.first_name
 
-    def __str__(self):
-        return self.first_name
+
+# def full_name(self):
+#         return f"{self.first_name}{self.last_name}"
+
+# def year_of_birth(self):
+#         return datetime.datetime.now().year-self.age
 
 
-    def full_name(self):
-        return f"{self.first_name}{self.last_name}"
+class Course(models.Model):
+   name_of_the_course=models.CharField(max_length=10,null=True)
+   course_code=models.CharField(max_length=20,null=True)
+   syllabus=models.CharField(max_length=10,null=True)
+   description=models.CharField(max_length=20,null=True)
+   course_unit=models.CharField(max_length=15,null=True)
+   course_duration=models.CharField(max_length=3,null=True)
+   course_material=models.CharField(max_length=18,null=True)
 
-    def year_of_birth(self):
-        return datetime.datetime.now().year-self.age    
-
+#    def __str__(self):
+#         return self.first_name
         
 
 
+
+class Trainer(models.Model):
+    first_name = models.CharField(max_length=10,null=True)
+    last_name=models.CharField(max_length=25,null=True)
+    email=models.EmailField(default=30,null=True)
+    phone_number=models.CharField(max_length=10,null=True)
+    course=models.CharField(max_length=20,null=True)
+    trainers_id=models.CharField(max_length=10,null=True)
+    no_of_lessons_attended=models.PositiveSmallIntegerField(default=1,null=True)
+    bank_account=models.CharField(max_length=15,null=True)
+    gender=models.CharField(max_length=15,null=True)
+    profile_trainer=models.CharField(max_length=20,null=True)
+    profession=models.CharField(max_length=20,null=True)
+    contact=models.CharField(max_length=15,null=True)
+    image=models.ImageField(default="img.jpeg")
+    cv=models.FileField(default="file.pdf")
+
+    
 
